105bb1113ae20d053934fbea88212e7ff4a5bf7d
ItzzDanii <barbuto.daniel29@hotmail.com>
2026-03-18 21:35:12 +0100

    Aggiunto riepilogo

--- Java - Discoteca/Main.java
+++ Java - Discoteca/Main.java
@@ -1,7 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        Persona p = new Persona();
+        Discoteca discoteca = new Discoteca();
 
-        System.out.println(p.toString());
+        discoteca.start();
+
+        try {
+            discoteca.join();
+        } catch (Exception e) {}
+
+        String riepilogo = 
+        "Drink acquistati: "+Singleton.getInstance().num_drink_acquistati +
+        "\nPersone con biglietto online: "+Singleton.getInstance().num_persone_online+
+        "\nPersone con biglietto loco: "+Singleton.getInstance().num_persone_loco+
+        "\nTempo medio in discoteca: "+Singleton.getInstance().num_persone_online+
+        "\nNumero persone metà serata: "+Singleton.getInstance().num_persone_metà_serata+
+        "\nGuadagno totale: "+Singleton.getInstance().guadagno_totale+"$";
+
+        System.out.println();
+        System.out.println("=== RIEPILOGO SERATA ===");
+        System.out.println(riepilogo);
     }
 }
